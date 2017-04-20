7ebdf01c53304d28f61c9d5e6fa1a0438d121f44
Oscar Ramirez <tuxskar@gmail.com>
2017-04-20 23:45:07 +0200

    Corrected spanish jokes using non-ascii characters and added one more.

--- pyjokes/jokes_es.py
+++ pyjokes/jokes_es.py
@@ -1,33 +1,28 @@
 # -*- coding: utf-8 -*-
 
 neutral = [
-    #'Se abre el ascensor y hay un programador dentro, le preguntan: - Sube o baja? A lo que el programador responde: - Si.',
-    'Se abre el ascensor y hay un programador dentro, le preguntan: - Sube o baja? A lo que el programador responde: - Si.',
-    #'Que le dice un bit al otro? Nos vemos en el bus.',
-    'Que le dice un bit al otro? Nos vemos en el bus.',
-    'Atracador: El dinero o la vida! Programador: Lo siento, soy programador. Atracador: Y? Programador: No tengo dinero ni vida.',
-    'Que es un terapeuta? - 1024 Gigapeutas',
-    'Cuantos programadores hacen falta para cambiar una bombilla? - Ninguno, porque es un problema de hardware.',
-    'Se abre el telon, aparece un informatico y dice: que habeis tocado que no se cierra el telon!',
-    '- ...fallece una persona mientras estudiaba en la biblioteca - Que estaba estudiando? - La API de Windows.',
-    'Que le dice un GIF a un JPG? -Animate hombre...',
-    'Que dice un informatico que se esta ahogando en la playa?: F1, F1!',
+    'Se abre el ascensor y hay un programador dentro, le preguntan: - Sube o baja? A lo que el programador responde: - Sí.',
+    '¿Que le dice un bit a otro? Nos vemos en el bus.',
+    'Atracador: ¡El dinero o la vida! Programador: Lo siento, soy programador. Atracador: ¿Y? Programador: No tengo dinero ni vida.',
+    '¿Qué es un terapeuta? - 1024 Gigapeutas',
+    '¿Cuántos programadores hacen falta para cambiar una bombilla? - Ninguno, porque es un problema de hardware.',
+    'Se abre el telon, aparece un informático y dice: ¡qué habeis tocado que no se cierra el telon!',
+    '- ...fallece una persona mientras estudiaba en la biblioteca - ¿Qué estaba estudiando? - La API de Windows.',
+    '¿Qué le dice un GIF a un JPG? -Animate hombre...',
+    '¿Qué dice un informatico que se esta ahogando en la playa?: ¡F1, F1!',
     'Solo hay 10 tipos de personas en el mundo, las que entienden binario y las que no.',
     'Solo hay 10 tipos de personas en el mundo: las que entienden trinario, las que no, y las que lo confunden con binario.',
     'Una mujer dice a su marido informatico: - Ve al supermercado y trae una barra de pan, y si hay huevos, doce. Trajo doce barras de pan.',
-    'Quien demonios es el general Failure, y que hace intentando leer de mi disco duro?',
-    'En que se diferencian Windows y un virus? En que el virus funciona, y es gratis.'
+    '¿Quién demonios es el general Failure, y que hace intentando leer de mi disco duro?',
+    '¿En que se diferencian Windows y un virus? En que el virus funciona, y es gratis.',
+    'Apostaría que cuando llegué al hotel traimos 4 maletas pero ahora solo cuento 3. 0, 1, 2 y 3'
 ]
 
 adult = [
-    #'Que es una mujer objeto? Un instancia de una mujer con clase',
-    #'- Cuantos dalmatas habia en la peli? - 101. - Por el culo te la hinco!',
-    #'Encuentran programador muerto en la ducha con un bote de champu: "enjabonar, aclarar y vuelta a empezar"',
-    #'Esto es un mainframe que le dice a un PC: "tan pequeno y ya "computas"?'
-    'Que es una mujer objeto? Un instancia de una mujer con clase',
-    '- Cuantos dalmatas habia en la peli? - 101. - Por el culo te la hinco!',
-    'Encuentran programador muerto en la ducha con un bote de champu: "enjabonar, aclarar y vuelta a empezar"',
-    'Esto es un mainframe que le dice a un PC: "tan pequeno y ya "computas"?'
+    '¿Qué es una mujer objeto? Una instancia de una mujer con clase',
+    '- ¿Cuantos dalmatas habia en la peli? - 101. - ¡Por el culo te la hinco!',
+    'Encuentran a un programador muerto en la ducha con un bote uuude champu: "enjabonar, aclarar y vuelta a empezar"',
+    'Esto es un mainframe que le dice a un PC: ¿"tan pequeño y ya "computas"?'
 ]
 
 jokes_es = {
